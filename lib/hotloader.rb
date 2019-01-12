11cdb5d8fc1b12fe7193d85d28066083fb784d23
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-01-12 21:28:34 +0000

    hotloadre v 1.0.7

--- lib/hotloader.rb
+++ lib/hotloader.rb
@@ -1,33 +1,33 @@
-require "hotloader/version"
+require 'hotloader/version'
 module Hotloader
   class Engine < ::Rails::Engine
     isolate_namespace Hotloader
+    if Rails.env.development?
+      initializer 'refresh', before: :load_config_initializers do |app|
+        Rails.application.routes.append do
+          mount ActionCable.server => '/cable'
+        end
 
-    initializer "refresh", before: :load_config_initializers do |app|
-      Rails.application.routes.append do
-        mount ActionCable.server => '/cable'
-      end
-      Rails.application.configure do 
+        Rails.application.configure do
           ActionCable.server.config.logger = Logger.new(nil)
-      end
+        end
 
-      Hotloader.load_files.each do |file|
-        require_relative File.join("../", file)
-      end
+        Hotloader.load_files.each do |file|
+          require_relative File.join('../', file)
+        end
 
-      app.config.assets.precompile << "hotloader.js"
+        app.config.assets.precompile << 'hotloader.js'
 
-      listener = Listen.to("app") do 
-        ActionCable.server.broadcast "refresh_channel", {title: "refresh"}
+        listener = Listen.to('app') do
+          ActionCable.server.broadcast 'refresh_channel', { title: "refresh" }
+        end
+        listener.start
       end
-      listener.start
     end
   end
   def self.load_files
     [
-      "app/channels/refresh_channel.rb"
+      'app/channels/refresh_channel.rb'
     ]
-  end    
+  end
 end
-
-
